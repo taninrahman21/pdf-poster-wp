dd9f97ab67a6817c4308e5590b2abe0d7d4a49b5
Tanin Rahman <taninrahman@Tanins-Mac-mini.local>
2026-05-16 15:26:14 +0600

    fix existing css issues

--- build/blocks/pdf-poster/index.asset.php
+++ build/blocks/pdf-poster/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fe0fba9e8f1e917ccc5f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '50dbadbe405caa264f33');
