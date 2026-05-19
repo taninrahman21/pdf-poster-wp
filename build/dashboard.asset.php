edd6bde274c2244a9dcb01efb0cd582abdeb71cf
Tanin Rahman <taninrahman@Tanins-Mac-mini.local>
2026-05-19 16:40:53 +0600

    wp v2.5.2

--- build/dashboard.asset.php
+++ build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data'), 'version' => 'fcbb3736aeac4dfa530e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data'), 'version' => '7554d8d43e94073039cc');
